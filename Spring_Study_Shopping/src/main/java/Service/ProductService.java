bc60e0ca68d9a77628e67b243fde945688eabd58
SangHyuk <tkdgur1730@gmail.com>
2025-05-26 23:48:02 +0900

    product 클래스 관련 서비스 클래스와 컨트롤러 클래스 구성

--- Spring_Study_Shopping/src/main/java/Service/ProductService.java
+++ Spring_Study_Shopping/src/main/java/Service/ProductService.java
@@ -2,17 +2,32 @@ package Service;
 
 import Products.Product;
 import Repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService {
-    private final ProductRepository productRepository;
+    private final ProductRepository productRepository;//실제 db와 데이터를 주고받는 객체
 
+    @Autowired
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
-    public void saveProduct(Product product) {
+    public void saveProduct(Product product) {//jparepository에 구현된 save메서드 호출
         productRepository.save(product);
+    }
+
+    public Product save(Product product) {
+        return productRepository.save(product);
+    }
+
+    public List<Product> findAll(){
+        return productRepository.findAll();
+    }
+    public Product findById(Long id) {
+        return productRepository.findById(id).orElseThrow(() -> new RuntimeException("상품 없음"));
     }
 }
