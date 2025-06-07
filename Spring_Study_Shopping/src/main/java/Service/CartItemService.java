82d326b2cf8d7ce694a67bdfa206fe0847ff0e63
SangHyuk <tkdgur1730@gmail.com>
2025-06-07 19:38:50 +0900

    user와 cartitem 연동

--- Spring_Study_Shopping/src/main/java/Service/CartItemService.java
+++ Spring_Study_Shopping/src/main/java/Service/CartItemService.java
@@ -3,15 +3,21 @@ package Service;
 import Items.CartItem;
 import Products.Product;
 import Repository.CartItemRepository;
+import Repository.UserRepository;
+import Users.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class CartItemService {
     private final CartItemRepository cartItemRepository;
+    private final UserRepository userRepository;
 
-    public CartItemService(CartItemRepository cartItemRepository) {
+    public CartItemService(CartItemRepository cartItemRepository, UserRepository userRepository) {
         this.cartItemRepository = cartItemRepository;
+        this.userRepository = userRepository;
     }
 
     public CartItem save(CartItem cartItem) {
@@ -23,5 +29,11 @@ public class CartItemService {
     }
     public CartItem findById(Long id) {
         return cartItemRepository.findById(id).orElseThrow(() -> new RuntimeException("상품 없음"));
+    }
+
+    public CartItem saveCartItem(CartItem cartItem, Long userId) {
+        User user = userRepository.findById(userId) .orElseThrow(() -> new RuntimeException("User not found with id: " + userId));
+        cartItem.setUser(user);
+        return cartItemRepository.save(cartItem);
     }
 }
